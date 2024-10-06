48e9d8845d2f81e8c50efa895f79a285fc589cf8
Erika <49363926+givemethecodo@users.noreply.github.com>
2024-10-05 19:44:38 -0700

    organizational things

--- toggle.js
+++ toggle.js
@@ -3,3 +3,13 @@ function switchMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+ 
+function updateTimeDisplay(value) {
+    let hours = Math.floor(value / 60);  // Get the hours
+    let minutes = value % 60;  // Get the remaining minutes
+    // Pad the hours and minutes with leading zeros if necessary
+    let formattedTime = String(hours).padStart(2, '0') + ':' + String(minutes).padStart(2, '0');
+    
+    // Update the display
+    document.getElementById('rangeValue').innerText = formattedTime;
+}
